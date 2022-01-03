451d16c22fdfbe38cd4335ed72fa6a6f3e2aac47
Gregório Neto <igregoriodoneto@outlook.com>
2022-01-03 17:39:34 -0300

    project beginner ball

---
+++ Readme.md
@@ -0,0 +1,5 @@
+#Beginner Ball Gamer
+Mini-Projeto Desenvolvido iniciando aos estudos de Desenvolvimento de jogos.
+
+### Características
+Personagem se move e pula na plataforma. Mostrando a base da física dos jogos.
---
+++ beginner-ball/Assets/player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Player attributes
+    public float speed;
+    public float jumpForce;
+
+    public Rigidbody2D rigigBody;
+
+    float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumpPlayer();
+    }
+
+    void FixedUpdate()
+    {
+        moviePlayer();
+        
+    }
+
+    void moviePlayer()
+    {
+        direction = Input.GetAxis("Horizontal");
+
+        rigigBody.velocity = new Vector2(direction * speed,rigigBody.velocity.y);
+    }
+
+    void jumpPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigigBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
